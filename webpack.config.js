2e5dca7e119252746d1d19da333653df8b11218b
AndreyKuryukin <akuryukin@wiley.com>
2020-02-07 23:16:32 +0300

    Minimal react app

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/app/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.js'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8080
+    },
+    module: {
+        rules: [
+            { test: /\.js(x)$/, use: 'babel-loader' },
+        ]
+    },
+    plugins: [
+        new CopyPlugin([
+          { 
+            from: path.join(__dirname, '/src/static/index.html'), 
+          },
+        ]),
+      ],
+  };
