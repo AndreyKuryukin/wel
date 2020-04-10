3216e1aa5a89f0a8eee3a575a4a228c32dde51cd
Farid Ismailov fismailov@wiley.com <fismailov@wiley.com>
2020-04-10 12:51:53 +0300

    TopicScore element created

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Comp = (props) => {
-    return <div>
-        <span>Hello</span>
-        <h2>{props.word}</h2>
-    </div>
-};
+const topics = [{
+  name: 'topic name',
+  completion: 80,
+  range: [50, 60]
+}];
 
-ReactDOM.render(<Comp word="World"/>, document.getElementById('root'));
+class Completion extends React.Component {
+  render() {
+    const completion = this.props.completion;
+    return <div>
+      <span>{completion}</span>
+      <div style={{height: 140, width: 18, background: "#cecece", display: "flex", flexDirection: "column-reverse"}}>
+        <div style={{height: `${completion}%`, background: "#24a202"}}/>
+      </div>
+    </div>
+  }
+}
+
+class Topic extends React.Component {
+  render() {
+    const {name, completion, range} = this.props;
+    return <div>
+      <div className="charts">
+        <Completion completion={completion}/>
+        {/*<Range range={range}/>*/}
+      </div>
+      <span>{name}</span>
+    </div>
+  }
+}
+
+class TopicScore extends React.Component {
+  render() {
+    return <div>
+      {topics.map(topic => <Topic {...topic}/>)}
+    </div>
+  }
+}
+
+ReactDOM.render(<TopicScore/>, document.getElementById('root'));
