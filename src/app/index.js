203c15830b0c4aae063d046458fa8cf0945584c4
Farid Ismailov fismailov@wiley.com <fismailov@wiley.com>
2020-04-10 14:58:15 +0300

    homework-1: done

--- src/app/index.js
+++ src/app/index.js
@@ -1,40 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const topics = [{
-  name: 'topic name',
-  completion: 80,
-  range: [50, 60]
-}];
+const topics = [
+  {
+    name: 'Ethical and Professional Standards (1)',
+    completion: 90,
+    range: [80, 90]
+  },
+  {
+    name: 'Ethical and Professional Standards (2)',
+    completion: 75,
+    range: [70, 80]
+  },
+  {
+    name: 'Quantitative Methods',
+    completion: 40,
+    range: [35, 45]
+  },
+  {
+    name: 'Economics',
+    completion: 10,
+    range: [10, 30]
+  },
+];
+
+class Range extends React.Component {
+  render() {
+    const range = this.props.range;
+    const bottom_start = 140 * range[0] / 100;
+    return <div className="range_container">
+      <span className="percentage">{range[0]}-{range[1]}</span>
+      <div className="range_bar">
+        <div className="range_fill" style={{bottom: `${bottom_start}px`, height: `${range[1] - range[0]}%`}}/>
+      </div>
+    </div>
+  }
+}
 
 class Completion extends React.Component {
   render() {
     const completion = this.props.completion;
-    return <div>
-      <span>{completion}</span>
-      <div style={{height: 140, width: 18, background: "#cecece", display: "flex", flexDirection: "column-reverse"}}>
-        <div style={{height: `${completion}%`, background: "#24a202"}}/>
+    return <div className="completion_container">
+      <span className="percentage">{completion}</span>
+      <div className="completion_bar">
+        <div className="completion_fill" style={{height: `${completion}%`, background: `${calculateColor(completion)}`}}/>
       </div>
     </div>
+  }
+}
+
+function calculateColor(percentage) {
+  switch (true) {
+    case percentage <= 20:
+      return "#e94221";
+    case percentage <= 50:
+      return "#f38a00";
+    case percentage <= 75:
+      return "#ffd600";
+    case percentage <= 100:
+      return "#5aba47";
   }
 }
 
 class Topic extends React.Component {
   render() {
     const {name, completion, range} = this.props;
-    return <div>
+    return <div className="topic">
       <div className="charts">
         <Completion completion={completion}/>
-        {/*<Range range={range}/>*/}
+        <Range range={range}/>
       </div>
-      <span>{name}</span>
+      <span className="title">{name}</span>
     </div>
   }
 }
 
 class TopicScore extends React.Component {
   render() {
-    return <div>
+    return <div className="topics_container">
       {topics.map(topic => <Topic {...topic}/>)}
     </div>
   }
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,28 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/app/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'app.js'
-    },
+  entry: './src/app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
 
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 8080
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, use: 'babel-loader' },
-        ]
-    },
-    plugins: [
-        new CopyPlugin([
-          { 
-            from: path.join(__dirname, '/src/static/index.html'), 
-          },
-        ]),
-      ],
-  };
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, use: 'babel-loader'},
+    ]
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: path.join(__dirname, '/src/static/index.html'),
+        from: path.join(__dirname, '/src/static/index.css'),
+      },
+    ]),
+  ],
+};
