b35a56c70c405cd9c730190dee217f5a4a2d6e61
Tatiana Ermakova <46784317+termakova@users.noreply.github.com>
2020-04-10 17:53:10 +0300

    simple component created

--- src/app/index.js
+++ src/app/index.js
@@ -1,43 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.scss';
 
-const topics = [{
-  name: 'topic name',
-  completion: 80,
-  range: [50, 60]
-}];
-
-class Completion extends React.Component {
-  render() {
-    const completion = this.props.completion;
-    return <div>
-      <span>{completion}</span>
-      <div style={{height: 140, width: 18, background: "#cecece", display: "flex", flexDirection: "column-reverse"}}>
-        <div style={{height: `${completion}%`, background: "#24a202"}}/>
-      </div>
-    </div>
-  }
+class Button extends React.Component {
+  constructor() {
+    super(...arguments);
+                }
+   render() {
+   return <button className='clickButton' onClick={() => {this.props.callBack()}}> {this.props.title} </button>
+   }
 }
 
-class Topic extends React.Component {
-  render() {
-    const {name, completion, range} = this.props;
-    return <div>
-      <div className="charts">
-        <Completion completion={completion}/>
-        {/*<Range range={range}/>*/}
-      </div>
-      <span>{name}</span>
-    </div>
-  }
+class HelloWorld extends React.Component {
+    constructor() {
+    super(...arguments);
+    this.state = {text: 'HELLO WORLD', buttonProps: {callBack: () => this.setState({text: 'Clicked'})}};
+    }
+    render() {
+        return <div>
+        <span> {this.state.text} </span>
+        <Button title={this.state.text} {...this.state.buttonProps}/>
+        </div>
+    }
 }
 
-class TopicScore extends React.Component {
-  render() {
-    return <div>
-      {topics.map(topic => <Topic {...topic}/>)}
-    </div>
-  }
-}
 
-ReactDOM.render(<TopicScore/>, document.getElementById('root'));
+
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/app/index.js',
@@ -7,7 +8,6 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
       filename: 'app.js'
     },
-
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
@@ -16,9 +16,20 @@ module.exports = {
     module: {
         rules: [
             { test: /\.js$/, use: 'babel-loader' },
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
         ]
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
         new CopyPlugin([
           { 
             from: path.join(__dirname, '/src/static/index.html'), 
