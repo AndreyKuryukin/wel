7972a2c492d37922cf92fcc01ed452a8d5defbeb
Tatiana Ermakova <46784317+termakova@users.noreply.github.com>
2020-04-27 12:58:11 +0300

    practice

--- src/app/example.js
+++ src/app/example.js
@@ -1,16 +1,28 @@
 import React from "react";
+import "babel-polyfill";
 
 class Example extends React.Component {
 
     constructor() {
         super();
         this.state = {};
+        this.fetchData().then((data) => this.setState(data));
+        console.log('constructor');
+    }
 
-        Promise.all([
+    async fetchData() {
+        /*Promise.all([
             this.getData('/user'),
             this.getData('/userInfo'),
             this.getData('/userStatus')
-        ]).then(([user, info, status]) => this.setState({...user, ...info, ...status}));
+        ]).then(([user, info, status]) => this.setState({...user, ...info, ...status}));*/
+        const user = await this.getData('/user');
+        console.log(user);
+        const userInfo = await this.getData(`/userInfo?infoId=${user.infoId}`);
+        console.log(userInfo);
+        const userStatus = await this.getData('/userStatus');
+        console.log(userStatus);
+        return {...user, ...userInfo, ...userStatus};
     }
 
     getData(url) {
