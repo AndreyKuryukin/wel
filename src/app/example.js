07b327dbf11551c53e9d7a8112a2d2aaeef747a0
Tatiana Ermakova <46784317+termakova@users.noreply.github.com>
2020-05-14 19:08:57 +0300

    chain of promises completed

--- src/app/example.js
+++ src/app/example.js
@@ -2,52 +2,37 @@ import React from "react";
 import styles from './example.scss';
 import classnames from 'classnames';
 
-//  HOME WORK
-// Use lifecycle methods !!!
-// fetch('/user'); // {id: '', name: ''};
-// fetch('/userInfo'); // {tel, address};
-//
-// render() {
-//     const user = this.state.user;
-//     return <div>
-//         <span>Name: {`${user.name}`}</span>
-//         <span>Tel. :{`${user.tel}`}</span>
-//         <span>Address: {`${user.name}`}</span>
-//     </div>
-// }
-
 class Example extends React.Component {
 
     constructor() {
         super();
-
-        this.initPromise()
-            .then(result => `${result}-PROCCESSED`)
-            .then(this.processPromise)
-            .then(console.log)
-            .catch((e) => {
-
-            })
         this.state = {};
+
+        Promise.all([
+            this.getData('/user'),
+            this.getData('/userInfo'),
+            this.getData('/userStatus')
+        ]).then(([user, info, status]) => this.setState({...user, ...info, ...status}));
     }
 
-    initPromise() {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve('RESOLVED');
-            }, 5000);
-        });
+    getData(url) {
+        return fetch(url).then(data => data.json());
     }
 
+    getColor() {
+        if (this.state.status === 'OK') {
+            return 'green';
+        } else return 'red';
+    }
 
     render() {
-        const {result} = this.state
-        console.log('Render');
-        return <div>
-            <span>{result}</span>
+        return <div style={{backgroundColor: this.getColor()}}>
+            <div>User Name: {this.state.name}</div>
+            <div>User Tel: {this.state.tel}</div>
+            <div>User Address: {this.state.address}</div>
+            <div>User Status: {this.state.status}</div>
         </div>
     }
-
 }
 
 export default Example;
